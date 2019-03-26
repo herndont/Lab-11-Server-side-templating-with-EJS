f5979d8e103bec584ce8c15e0dfc900c70a86a76
herndont <herndontrey@gmail.com>
2019-03-26 10:16:30 -0700

    server.json work

---
+++ server.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+
+const app = express();
+const PORT = process.env.PORT;
+
+//Middleware
+app.use(express.urlencoded({ extended : true}));
+app.use(express.static('public'));
+
+//Set the view engine for server-side templating
+app.set('view engine', 'ejs');
+
+//API Routes
+//Render the  search form
+app.get('/', newSearch);
+
+//creates a new search to Google Books API
+app.post('/searches', createSearch);
+
+//Catch-all
+app.get('*', (request, response) => response.status(404).send('Get the HELL out of here NOW!'));
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
+
+//Helper  
+function book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+
+  this.title = info.title || 'No Title Available';
+
+}
+
+//Note that ejs file is not required
+function newSearch(request, response) {
+  response.render('pages/index');
+}
+
+//No API key required
+//console.log request.body and request.body.search
+function createSearch(request, response) {
+  let url = 'https:///www.googleapis.com/books/v1/volumes?q=';
+
+  console.log(request.body);
+
+  if (request.body.search[1] === 'title') {url += `+intitle:${request.body.search[0]}`;}
+  if (request.body.search[1] === 'author') {url += `+inauthor:${request.body.seach[0]}`;}
+
+  console.log(url);
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', {searchResults: results}));
+}
+
